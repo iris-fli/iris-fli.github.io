d14db084f9a458a0755fadbad6801e126f06a6cb
iris-fli <fli25@dons.usfca.edu>
2022-02-05 01:04:08 -0800

    try to fix app test2

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Iris/i);
+  const linkElement = screen.getByText(/I'm Iris/i);
   expect(linkElement).toBeInTheDocument();
 });
